864f8e4b18d90c31211173d8b83c8f655b597fd8
PatiSacramento <patriciargsacramento@outlook.com>
2022-11-25 20:46:42 -0300

    variables and input

---
+++ main.py
@@ -0,0 +1,65 @@
+
+
+#  single line commenter
+
+"""
+in block commentary
+"""
+
+"""
+types of data:
+    string - str("text")
+    number - integer / float (fraction)
+    boolean - bool
+    
+changing data: 
+    str() -> to string
+    int() -> to integer
+    float() -> to float
+    
+x = str(2)
+y = int(5)
+z = float(10)
+
+print(x + x)
+print(y + y)
+print(z + z)
+   
+"""
+
+"""
+nome = input("Qual é o seu nome? ")
+idade = input("Qual a sua idade? ")
+cidade = input("Em que cidade você mora? ")
+
+# não aceita imprimir dois tipos de dados diferentes
+print('A ' + nome + ' tem ' + idade + ' anos e mora em ' + cidade)
+
+"""
+
+"""
+ano_nascimento = int(input("Em que ano você nasceu? "))
+print(type(ano_nascimento))
+ano_atual = 2022
+
+idade = ano_atual - ano_nascimento
+
+print("Sua idade é: " + str(idade))
+
+"""
+
+"""
+fruta = "Abacate"
+
+print(fruta[-1]) # conta de trás pra frente
+
+valor = str(99.75)
+
+
+print(valor[3:])  # da posição 3 até o final
+print(valor[:3])  # da posição inicial até a posição 3
+"""
+
+
+
+
