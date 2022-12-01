6d00feca5734597242bc3160d1890e3494a050a8
PatiSacramento <patriciargsacramento@outlook.com>
2022-12-01 11:49:43 -0300

    concatenating lists

---
+++ data_structure.py
@@ -0,0 +1,44 @@
+# Data structure
+
+"""
+lists and methods
+
+cidades = ['Belo Horizonte', 'São Paulo', 'Rio de Janeiro']
+
+print(cidades)
+
+print(cidades[-1]) # negativo -> de trás pra frente. -1 é o último da lista
+
+
+cidades.append('Fortuna de Minas') # = push js
+cidades.remove('São Paulo')
+cidades.insert(1, 'Porto Alegre') # insere na posição desejada
+cidades.pop(0) # tira o elemento da posição indicada
+cidades.sort()
+print(cidades)
+"""
+
+
+"""
+concatenating lists
+
+
+numeros = [1, 2, 3, 4, 5]
+letras = ['a', 'b', 'c', 'd', 'e']
+
+# final = numeros * 2 # multiplica a lista de acordo com o número que você pedir -> [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+
+# final = numeros + letras # concatena os dois arrays
+
+# outra opção é
+
+numeros.extend(letras)
+print(numeros)
+
+itens = [['item1', 'item2'], ['item3', 'item4']]
+
+print(itens[1][0])
+"""
+
+
+
